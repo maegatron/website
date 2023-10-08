fc0d2c2cbb42b2d4cb426b0e5a8173f40c1e604b
maegatron <mejparales@gmail.com>
2023-10-08 17:24:39 +0800

    created navigation bar

--- admin_website/pages/index.js
+++ admin_website/pages/index.js
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useSession, signIn, signOut } from "next-auth/react"
 
+import Nav from '@/components/Nav'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -17,6 +19,12 @@ export default function Home() {
      )
   }
   return (
-   <div>logged in {session.user.email}</div>
+    <div className='bg-blue-900 min-h-screen flex'>
+      <Nav />
+      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
+        logged in {session.user.email}
+      </div>
+    </div>
+   
   )
 }
