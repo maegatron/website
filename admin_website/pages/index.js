5b77c56205fc89a70b5af3641a68425dd38f35c0
maegatron <mejparales@gmail.com>
2023-10-08 17:45:08 +0800

    added layout for index.js

--- admin_website/pages/index.js
+++ admin_website/pages/index.js
@@ -1,30 +1,12 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useSession, signIn, signOut } from "next-auth/react"
-
-import Nav from '@/components/Nav'
+import Layout from '@/components/Layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const { data: session } = useSession()
-
-  if (!session){
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-         <div className='text-center w-full'>
-           <button onClick={()=> signIn('google')} className='bg-white p-2 rounded-lg'>Login with Google</button>
-         </div>
-      </div>
-     )
-  }
-  return (
-    <div className='bg-blue-900 min-h-screen flex'>
-      <Nav />
-      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
-        logged in {session.user.email}
-      </div>
-    </div>
-   
+  return(
+    <Layout>test</Layout>
   )
 }
