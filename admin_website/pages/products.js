c36c56ab7c2a21cc08e59a8211c3a2f722a01853
maegatron <mejparales@gmail.com>
2023-10-08 20:16:06 +0800

    add router to products/new

--- admin_website/pages/products.js
+++ admin_website/pages/products.js
@@ -1,6 +1,12 @@
-import Layout from '@/components/Layout'
-export default function Products(){
-    return (
-        <Layout>products page</Layout>
-    )
+import Link from "next/link";
+import Layout from "@/components/Layout";
+
+export default function Products() {
+  return (
+    <Layout>
+      <Link href={"/products/new"} className="bg-blue-100 text-blue-900 py-2 px-2 rounded-full">
+        Add product
+      </Link>
+    </Layout>
+  );
 }
